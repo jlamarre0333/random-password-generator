8744b7db1629262e40765c3b589e287e61efbd91
Jake Lamarre <jakelamarre@hotmail.com>
2021-03-05 20:21:11 -0500

    finished project and formatting

--- script.js
+++ script.js
@@ -5,34 +5,40 @@ import { alphabet, symbols } from './data.js'
 
 
 var generatePassword = function () {
-  
+
   // Declared variabls
-  var criteria = window.prompt("Which criteria would you like to include in your password? Type 'SPECIAL CHARACTERS', 'UPPERCASE', 'LOWERCASE', 'NUMERIC' ").toLowerCase(); 
+  var criteria = window.prompt("Which criteria would you like to include in your password? Type 'SPECIAL CHARACTERS', 'UPPERCASE', 'LOWERCASE', 'NUMERIC' ").toLowerCase();
   var length = parseInt(window.prompt("Choose a length for your password between 8 characters and 128."));
   var randomNumber = Math.floor(Math.random() * 90 + 10)
   var randomSymbol = symbols[Math.floor(Math.random() * symbols.length - 1)]
   console.log(length, criteria, randomSymbol)
 
   // Verify Length is between 8 and 128
-
   if (length < 8) {
     window.alert("Length must be greater than 8 characters!");
     generatePassword();
-  } else if(length > 128){
+  } else if (length > 128) {
     window.alert("Length must be lower than 128 characters!");
     generatePassword();
   }
 
   //generate random alphabet string off of user input lenght
-
-  let password = ""
+  let password = "";
   for (let i = 0; i < length; i++) {
-    var randomLetter = alphabet[Math.floor(Math.random() * 25)]
-    password = password + randomLetter;
-
+    var isLetter = Math.random() > .5;
+    if (isLetter) {
+      var randomLetter = alphabet[Math.floor(Math.random() * 25)]
+      console.log(randomLetter, "random letter");
+      password = password + randomLetter;
+    } else {
+      var randomSymbol = symbols[Math.floor(Math.random() * 5)]
+      console.log(randomSymbol, "random symbol");
+      password = password + randomSymbol;
+    } console.log(password, "password inside for loop");
   }
+  console.log(password, "password when finished");
 
-  
+
   // if numeric add number to password 
   if (criteria.includes("numeric")) {
     //then add numbers to end of random password
@@ -45,16 +51,13 @@ var generatePassword = function () {
     var shortpass = password.substring(0, password.length - 1);
     password = shortpass + randomSymbol;
   }
-  // if uppercase return uppercase password
 
+  // if uppercase return uppercase password
   if (criteria.includes("uppercase")) {
     return password.toUpperCase();
   }
 
-  //if lowercase lowercase is default from data.js and will by default return lowercase
-
-  
-
+  //if lowercase, lowercase is default from data.js and will by default return lowercase
   return password
 }
 
@@ -65,9 +68,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
